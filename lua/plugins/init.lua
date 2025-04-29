7b6bfba5e420ca1bc4afb801a79ceeed22eb897f
rflihmmm <indomilk04@gmail.com>
2025-04-29 12:35:54 +0800

    add lazydocker and avante

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -93,4 +93,102 @@ return {
             { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
         },
     },
+    -- lazydocker.nvim
+    {
+        "mgierada/lazydocker.nvim",
+        dependencies = { "akinsho/toggleterm.nvim" },
+        config = function()
+            require("lazydocker").setup({
+                border = "curved", -- valid options are "single" | "double" | "shadow" | "curved"
+            })
+        end,
+        event = "BufRead",
+        keys = {
+            {
+                "<leader>ld",
+                function()
+                    require("lazydocker").open()
+                end,
+                desc = "Open Lazydocker floating window",
+            },
+        },
+    },
+    {
+        "yetone/avante.nvim",
+        event = "VeryLazy",
+        version = false, -- Never set this value to "*"! Never!
+        opts = {
+            -- add any opts here
+            provider = "openrouter",
+            vendors = {
+                deepseek = {
+                    __inherited_from = "openai",
+                    endpoint = "https://openrouter.ai/api/v1",
+                    api_key_name = "OPENROUTER_API_KEY",
+                    model = "deepseek/deepseek-r1",
+                },
+            }, -- for example
+        },
+        -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+        build = "make",
+        -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "stevearc/dressing.nvim",
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            --- The below dependencies are optional,
+            "echasnovski/mini.pick", -- for file_selector provider mini.pick
+            "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+            "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+            "ibhagwan/fzf-lua", -- for file_selector provider fzf
+            "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+            "zbirenbaum/copilot.lua", -- for providers='copilot'
+            {
+                -- support for image pasting
+                "HakonHarnes/img-clip.nvim",
+                event = "VeryLazy",
+                opts = {
+                    -- recommended settings
+                    default = {
+                        embed_image_as_base64 = false,
+                        prompt_for_file_name = false,
+                        drag_and_drop = {
+                            insert_mode = true,
+                        },
+                        -- required for Windows users
+                        use_absolute_path = true,
+                    },
+                },
+            },
+            {
+                -- Make sure to set this up properly if you have lazy=true
+                "MeanderingProgrammer/render-markdown.nvim",
+                opts = {
+                    file_types = { "markdown", "Avante" },
+                },
+                ft = { "markdown", "Avante" },
+            },
+        },
+    },
+    -- lazydocker.nvim
+    {
+        "mgierada/lazydocker.nvim",
+        dependencies = { "akinsho/toggleterm.nvim" },
+        config = function()
+            require("lazydocker").setup({
+                border = "curved", -- valid options are "single" | "double" | "shadow" | "curved"
+            })
+        end,
+        event = "BufRead",
+        keys = {
+            {
+                "<leader>ld",
+                function()
+                    require("lazydocker").open()
+                end,
+                desc = "Open Lazydocker floating window",
+            },
+        },
+    },
 }
