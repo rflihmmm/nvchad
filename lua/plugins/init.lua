6ae8872a9a9af103d122553de56ceda522be43ad
rflihmmm <indomilk04@gmail.com>
2025-04-29 12:36:13 +0800

    delete studipidity

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -171,24 +171,4 @@ return {
             },
         },
     },
-    -- lazydocker.nvim
-    {
-        "mgierada/lazydocker.nvim",
-        dependencies = { "akinsho/toggleterm.nvim" },
-        config = function()
-            require("lazydocker").setup({
-                border = "curved", -- valid options are "single" | "double" | "shadow" | "curved"
-            })
-        end,
-        event = "BufRead",
-        keys = {
-            {
-                "<leader>ld",
-                function()
-                    require("lazydocker").open()
-                end,
-                desc = "Open Lazydocker floating window",
-            },
-        },
-    },
 }
